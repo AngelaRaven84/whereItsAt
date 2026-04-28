bbca3ce7c53b9d29d66a99763bb05f30769dcd41
Sara B <saraberg123@gmail.com>
2026-04-28 14:38:54 +0200

    Fixade headern och landningssidam

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from 'react-router-dom';
-//import Header from '../components/Header/Header';
+import Header from '../components/Header/Header';
 import FooterNav from '../components/FooterNav/FooterNav';
 
 function RootLayout() {
 	return (
 		<>
+			<Header />
 			<main>
 				<Outlet />
 			</main>
