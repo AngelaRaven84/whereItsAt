63d3a3447feb4d7a309cc26a3bd750e33b45a725
Sara B <saraberg123@gmail.com>
2026-04-30 14:13:36 +0200

    Fixat EventDetails med lite subtle animation och designat den efter figmaskissen. Även lagt till header och footer på alla sidor utom startsidan

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,9 +1,16 @@
 import { Outlet, useLocation } from 'react-router-dom';
+import Header from '../components/Header/Header';
+import FooterNav from '../components/FooterNav/FooterNav';
 
 function RootLayout() {
+	const location = useLocation();
+	const isStartPage = location.pathname === '/';
+
 	return (
 		<main>
+			{!isStartPage && <Header />}
 			<Outlet />
+			{!isStartPage && <FooterNav />}
 		</main>
 	);
 }
