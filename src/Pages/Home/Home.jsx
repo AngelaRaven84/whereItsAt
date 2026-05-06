2e0d595181257d3d3eef9a6cf884df0a866663f5
Sara B <saraberg123@gmail.com>
2026-05-06 21:58:23 +0200

    En sista fix, tog visst bort lite för mycket css utan att se mig för

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
 			<motion.section
 				className='container center home__content'
 				initial={{ opacity: 0, y: 96 }}
-				animate={{ opacity: 1, y: 1 }}
+				animate={{ opacity: 1, y: -24 }}
 				transition={{ duration: 0.5 }}>
 				<img src={logoIcon} alt="Where It's @ icon" className='home__icon' />
 
